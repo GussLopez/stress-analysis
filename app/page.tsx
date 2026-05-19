e15b4c69c0016783a6d7a0488057668bc6f0d87e
Gustavo <gustavolopez819@gmail.com>
2026-05-19 13:51:27 -0500

    feat(dashboard-kpis): enhance KPI display with additional metrics and icons

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import DashboardKpis from "@/src/features/dashboard/components/DashboardKpis";
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-screen-6xl w-full px-4 mx-auto">
       Dashboard
       <DashboardKpis />
     </div>
--- src/features/dashboard/components/DashboardKpis.tsx
+++ src/features/dashboard/components/DashboardKpis.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { useQuery } from "@tanstack/react-query"
+import { Angry, Frown, Info, Spotlight } from "lucide-react";
+import { FaRegFaceFlushed, FaRegFaceGrimace } from "react-icons/fa6";
 
 interface KpisData {
   avg_anger: number;
@@ -31,10 +33,66 @@ export default function DashboardKpis() {
   })
 
   return (
-    <div className="grid grid-cols-4 ">
+    <div className="grid grid-cols-5 gap-5">
       <div className="p-4 rounded-md shadow-xs border border-muted">
-        hello
+        <div className="flex items-center gap-2">
+          <div className="bg-neutral-100 p-1 rounded-md">
+            <Spotlight className="size-5" />
+          </div>
+          <span>Procentaje de Estrés</span>
+        </div>
+
+        <p className="mt-3 text-2xl font-medium">
+          {data?.stress_percentage} %
+        </p>
       </div>
+      <div className="p-4 rounded-md shadow-xs border border-muted">
+        <div className="flex items-center gap-2">
+          <div className="bg-neutral-100 p-1 rounded-md">
+            <Angry className="size-5" />
+          </div>
+          <span>Promedio de Ira</span>
+        </div>
+
+        <p className="mt-3 text-2xl font-medium">
+          {data?.avg_anger}
+        </p>
+      </div>
+      <div className="p-4 rounded-md shadow-xs border border-muted">
+        <div className="flex items-center gap-2">
+          <div className="bg-neutral-100 p-1 rounded-md">
+            <FaRegFaceGrimace className="size-5" />
+          </div>
+          <span>Promedio de Ansiedad</span>
+        </div>
+
+        <p className="mt-3 text-2xl font-medium">
+          {data?.avg_anxiety}
+        </p>
+      </div>
+      <div className="p-4 rounded-md shadow-xs border border-muted">
+        <div className="flex items-center gap-2">
+          <div className="bg-neutral-100 p-1 rounded-md">
+            <Frown className="size-5" />
+          </div>
+          <span>Promedio de Tristesa</span>
+        </div>
+        <p className="mt-3 text-2xl font-medium">
+          {data?.avg_sadness}
+        </p>
+      </div>
+      <div className="p-4 rounded-md shadow-xs border border-muted">
+        <div className="flex items-center gap-2">
+          <div className="bg-neutral-100 p-1 rounded-md">
+            <Frown className="size-5" />
+          </div>
+          <span>Promedio de </span>
+        </div>
+        <p className="mt-3 text-2xl font-medium">
+          {data?.avg_anger}
+        </p>
+      </div>
+
     </div>
   )
 }
