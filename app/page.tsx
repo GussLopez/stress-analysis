881529c00c4113f9fa70e5bc1d9db7f3607470ea
Gustavo <gustavolopez819@gmail.com>
2026-05-19 14:07:53 -0500

    feat(dashboard-kpis): update KPI styles and add share icon for total posts

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import DashboardKpis from "@/src/features/dashboard/components/DashboardKpis";
 export default function Home() {
   return (
     <div className="max-w-screen-6xl w-full px-4 mx-auto">
-      Dashboard
       <DashboardKpis />
     </div>
   );
--- src/features/dashboard/components/DashboardKpis.tsx
+++ src/features/dashboard/components/DashboardKpis.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { useQuery } from "@tanstack/react-query"
-import { Angry, Frown, Info, Spotlight } from "lucide-react";
-import { FaRegFaceFlushed, FaRegFaceGrimace } from "react-icons/fa6";
+import { Angry, Frown, Spotlight } from "lucide-react";
+import { FaRegFaceGrimace } from "react-icons/fa6";
+import { IoShareSocial } from "react-icons/io5";
 
 interface KpisData {
   avg_anger: number;
@@ -33,63 +34,61 @@ export default function DashboardKpis() {
   })
 
   return (
-    <div className="grid grid-cols-5 gap-5">
-      <div className="p-4 rounded-md shadow-xs border border-muted">
+    <div className="grid grid-cols-5 gap-5 pt-4">
+      <div className="p-4 rounded-md shadow-xs border border-muted group">
         <div className="flex items-center gap-2">
-          <div className="bg-neutral-100 p-1 rounded-md">
-            <Spotlight className="size-5" />
+          <div className="bg-neutral-100 p-1 rounded-md group-hover:bg-primary/10 group-hover:text-primary transition-colors">
+            <Spotlight className="size-5.5" />
           </div>
           <span>Procentaje de Estrés</span>
         </div>
 
-        <p className="mt-3 text-2xl font-medium">
-          {data?.stress_percentage} %
-        </p>
+        <p className="mt-3 text-2xl font-medium">{data?.stress_percentage} %</p>
       </div>
-      <div className="p-4 rounded-md shadow-xs border border-muted">
+      <div className="p-4 rounded-md shadow-xs border border-muted group">
         <div className="flex items-center gap-2">
-          <div className="bg-neutral-100 p-1 rounded-md">
-            <Angry className="size-5" />
+          <div className="bg-neutral-100 p-1 rounded-md group-hover:bg-primary/10 group-hover:text-primary transition-colors">
+            <Angry className="size-5.5" />
           </div>
           <span>Promedio de Ira</span>
         </div>
 
         <p className="mt-3 text-2xl font-medium">
-          {data?.avg_anger}
+          {data?.avg_anger} <span className="text-xs font-normal text-muted-foreground">prom.</span>
         </p>
       </div>
-      <div className="p-4 rounded-md shadow-xs border border-muted">
+      <div className="p-4 rounded-md shadow-xs border border-muted group">
         <div className="flex items-center gap-2">
-          <div className="bg-neutral-100 p-1 rounded-md">
-            <FaRegFaceGrimace className="size-5" />
+          <div className="bg-neutral-100 p-1 rounded-md group-hover:bg-primary/10 group-hover:text-primary transition-colors">
+            <FaRegFaceGrimace className="size-5.5" />
           </div>
           <span>Promedio de Ansiedad</span>
         </div>
 
         <p className="mt-3 text-2xl font-medium">
-          {data?.avg_anxiety}
+          {data?.avg_anxiety} <span className="text-xs font-normal text-muted-foreground">prom.</span>
         </p>
       </div>
-      <div className="p-4 rounded-md shadow-xs border border-muted">
+      <div className="p-4 rounded-md shadow-xs border border-muted group">
         <div className="flex items-center gap-2">
-          <div className="bg-neutral-100 p-1 rounded-md">
-            <Frown className="size-5" />
+          <div className="bg-neutral-100 p-1 rounded-md group-hover:bg-primary/10 group-hover:text-primary transition-colors">
+            <Frown className="size-5.5" />
           </div>
           <span>Promedio de Tristesa</span>
         </div>
         <p className="mt-3 text-2xl font-medium">
-          {data?.avg_sadness}
+          {data?.avg_sadness} <span className="text-xs font-normal text-muted-foreground">prom.</span>
         </p>
       </div>
-      <div className="p-4 rounded-md shadow-xs border border-muted">
+      <div className="p-4 rounded-md shadow-xs border border-muted group">
         <div className="flex items-center gap-2">
-          <div className="bg-neutral-100 p-1 rounded-md">
-            <Frown className="size-5" />
+          <div className="bg-neutral-100 p-1 rounded-md group-hover:bg-primary/10 group-hover:text-primary transition-colors">
+            <IoShareSocial className="size-5.5" />
           </div>
-          <span>Promedio de </span>
+          <span>Publicaciones totales</span>
         </div>
         <p className="mt-3 text-2xl font-medium">
-          {data?.avg_anger}
+          {data?.total_posts} <span className="text-xs font-normal text-muted-foreground">posts</span>
         </p>
       </div>
 
