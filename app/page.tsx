686bb8662a8d21905da03f122130624e37b0680a
AR23393269 <23393269@utcancun.edu.mx>
2026-05-20 17:57:09 -0500

    header & footer

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,26 @@ import DashboardKpis from "@/src/features/dashboard/components/DashboardKpis";
 
 export default function Home() {
   return (
-    <div className="max-w-screen-6xl w-full px-4 mx-auto">
-      <DashboardKpis />
-      <div className="grid grid-cols-2 gap-10 mt-10">
-        <DashboardBarChart />
-        <DashboardHeatMap />
-      </div>
+    <div className="max-w-screen-6xl w-full px-4 mx-auto flex flex-col min-h-screen">
+
+      <header className="w-full py-6 text-center border-b">
+        <h1 className="text-2xl font-semibold">
+          Stress Analysis en subreddits
+        </h1>
+      </header>
+
+      <main className="flex-grow">
+        <DashboardKpis />
+        <div className="grid grid-cols-2 gap-10 mt-10">
+          <DashboardBarChart />
+          <DashboardHeatMap />
+        </div>
+      </main>
+
+      <footer className="w-full py-6 text-center border-t mt-10 text-sm text-gray-500">
+        Magus Corp™ 2026. Todos los derechos reservados.
+      </footer>
+
     </div>
   );
 }
