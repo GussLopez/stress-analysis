8d128fba4b8555ba1bf54b09890e549182d6292b
Gustavo <gustavolopez819@gmail.com>
2026-05-20 16:52:14 -0500

    feat(landign): added bar chart and heat map components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import DashboardBarChart from "@/src/features/dashboard/components/DashboardBarChart";
+import DashboardHeatMap from "@/src/features/dashboard/components/DashboardHeatMap";
 import DashboardKpis from "@/src/features/dashboard/components/DashboardKpis";
 
 export default function Home() {
   return (
     <div className="max-w-screen-6xl w-full px-4 mx-auto">
       <DashboardKpis />
+      <div className="grid grid-cols-2 gap-10 mt-10">
+        <DashboardBarChart />
+        <DashboardHeatMap />
+      </div>
     </div>
   );
 }
